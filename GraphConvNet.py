1d51fe68fc8bae73388467ac99a2560b4fee66b6
Urup93 <kasperurupreimer@hotmail.com>
2020-01-28 09:54:28 +0100

    Added test cases for greedyAlg

--- GraphConvNet.py
+++ GraphConvNet.py
@@ -14,15 +14,20 @@ class GraphConvNet:
 
 
     # f optimazation function
-    # b budget (kan udlades hvis set cover og ikke IM)
-    def greedyAlg(self, func, b):
-        S = list()
-        for i in range(b):
-            V = [v for v in self.g.nodes if v not in S]
-            f_scores = [func(self.g, V, v) for v in V]
-            v = np.argmax(f_scores)
-            S.append(V[v])
-        return S
+    def greedyAlg(self, func):
+        solution = set()
+        elements_covered = set()
+        universe = set([u for u,d in self.g.nodes(data=True) if d["bipartite"] == 1])
+        subsets = set([u for u, d in self.g.nodes(data=True) if d["bipartite"] == 0])
+        while elements_covered != universe:
+            elements_not_covered = [u for u in universe if u not in elements_covered]
+            available_subsets = [s for s in subsets if s not in solution]
+            scores_for_subsets = [func(elements_not_covered, s) for s in available_subsets]
+            idx = np.argmax(scores_for_subsets)
+            best_subset = available_subsets[idx]
+            [elements_covered.add(u) for u in self.g.neighbors(best_subset)]
+            solution.add(best_subset)
+        return solution
 
 
     def GCN(self, score, features, depth, Weight_mat, weight_vec):
--- test_GraphConvNet.py
+++ test_GraphConvNet.py
@@ -7,18 +7,25 @@ from GraphConvNet import GraphConvNet
 class TestGraphConvNet(TestCase):
     def setUp(self):
         G = nx.Graph()
-        G.add_nodes_from([2, 1, 4, 0, 3])
-        G.add_edges_from([(0, 2), (1, 0), (3, 2)])
+        G.add_nodes_from(["a","b","c"], bipartite = 0)
+        G.add_nodes_from([2, 1, 4, 0, 3], bipartite = 1)
+        G.add_edges_from([("a", 2), ("a", 1), ("a", 4)])
+        G.add_edges_from([("b", 2)])
+        G.add_edges_from([("c", 2), ("c", 0), ("c", 3)])
         self.gcn = GraphConvNet(G)
 
     def test_score_sum_of_neighbors(self):
-        self.assertEqual(self.gcn.f([0, 1, 2, 3, 4], 0), 2)
-        self.assertEqual(self.gcn.f([0, 2, 3, 4], 0), 1)
-        self.assertEqual(self.gcn.f([0, 3, 4], 0), 0)
-        self.assertEqual(self.gcn.f([0, 1, 3, 4], 4), 0)
+        self.assertEqual(self.gcn.f([0, 1, 2, 3, 4], "a"), 3)
+        self.assertEqual(self.gcn.f([0, 2, 3, 4], "a"), 2)
+        self.assertEqual(self.gcn.f([0, 3, 4], "b"), 0)
+        self.assertEqual(self.gcn.f([0, 1, 4], "c"), 1)
         pass
 
     def test_greedy_alg(self):
+        func = self.gcn.f
+        self.assertEqual(self.gcn.greedyAlg(func),set(["a", "c"]))
+        self.gcn.g.add_edges_from([("c", 4), ("c", 1)])
+        self.assertEqual(self.gcn.greedyAlg(func), set(["c"]))
         pass
 
     def test_gcn(self):
