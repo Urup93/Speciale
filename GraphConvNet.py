31d958f543ebd048412db8e329008db55e2176c3
Urup93 <kasperurupreimer@hotmail.com>
2020-01-29 15:42:26 +0100

    Added alot. Next is the network architecture

--- GraphConvNet.py
+++ GraphConvNet.py
@@ -3,7 +3,6 @@ import numpy as np
 import networkx as nx
 import random
 
-
 class GraphConvNet:
 
     def __init__(self, g):
@@ -61,15 +60,89 @@ class GraphConvNet:
             [elements_covered.add(u) for u in self.g.neighbors(chosen_subset)]
         return solution, gain
 
-
     def compute_marginal_gain(self, iterations):
         total_gain = {s: 0 for s in self.subsets}
         for i in range(iterations):
             gain = self.probalisticGreedyAlg(self.f)[1]
             total_gain = {s: gain[s] + total_gain[s] for s in self.subsets}
         marg_gain = {s: total_gain[s] / iterations for s in self.subsets}
-        print(marg_gain)
+        return marg_gain
 
 
-    def GCN(self, score, features, depth, Weight_mat, weight_vec):
+    def feature_degree(self):
+        """
+        Computes the proportion of elements in the universe that each subset covers, and outputs
+        a dictionary of all subsets corrosponding proportions.
+        :return: dictionary that maps a subset to its proportion of elements of covered in the graph.
+        """
+        number_of_elements = len(self.universe)
+        std_degrees = {s: self.g.degree[s]/number_of_elements for s in self.subsets}
+        return std_degrees
+
+
+    def feature_mean_uni_degrees(self):
+        """
+        For each subset in the graph, goes through all the elements it is covering and for each element
+        computes the proportion of subsets that covers it. Then takes the mean over all elements covered
+        by this subset, and outputs this as a feature for the subset.
+        :return: A dictionary mapping all subsets to the mean degree of its elements
+        """
+        mean_uni_degrees = {}
+        number_of_subsets = len(self.subsets)
+        for s in self.subsets:
+            sum = 0
+            for n in self.g[s]:
+                sum += self.g.degree[n]/number_of_subsets
+            mean = sum/self.g.degree[s]
+            mean_uni_degrees[s] = mean
+        return mean_uni_degrees
+
+
+    def feature_min_uni_degrees(self):
+        """
+        For each subset in the graph, find the element it covers with the lowest degree and output the
+        proportion of subset this element is covered by.
+        :return: dictionary mapping all subsets to the degree of its lowest connected neighbor.
+        """
+        min_uni_degree = {}
+        number_of_subsets = len(self.subsets)
+        for s in self.subsets:
+            min_degree = float('inf')
+            for n in self.g[s]:
+                cur = self.g.degree[n]
+                if min_degree > cur:
+                    min_degree = cur
+            min_uni_degree[s] = min_degree/number_of_subsets
+        return min_uni_degree
+
+
+    def features_to_tensors(self):
+        """
+        Converts features of a graph into a tensor matrix
+        :return: Pytorch tensor matrix
+        """
+        degree_dict = self.feature_degree()
+        mean_dict = self.feature_mean_uni_degrees()
+        min_dict = self.feature_min_uni_degrees()
+        data = torch.empty(len(self.subsets), 3)
+        i = 0
+        subset_to_row = {}
+        for s in self.subsets:
+            data[i, 0] = degree_dict[s]
+            data[i, 1] = mean_dict[s]
+            data[i, 2] = min_dict[s]
+            subset_to_row[s] = i
+            i += 1
+        return data, subset_to_row
+
+
+    def labels_to_tensors(self, labels_dict, subset_to_row):
+        labels = torch.empty(len(self.subsets),1)
+        for l in labels_dict:
+            idx = subset_to_row[l]
+            labels[idx] = labels_dict[l]
+        return labels
+
+    def GCN(self, labels, features, depth, Weight_mat, weight_vec):
+
         return 0
--- test_GraphConvNet.py
+++ test_GraphConvNet.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 import networkx as nx
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 from GraphConvNet import GraphConvNet
 
@@ -20,13 +20,12 @@ class TestGraphConvNet(TestCase):
         self.assertEqual(self.gcn.f([0, 2, 3, 4], "a"), 2)
         self.assertEqual(self.gcn.f([0, 3, 4], "b"), 0)
         self.assertEqual(self.gcn.f([0, 1, 4], "c"), 1)
-        pass
+
 
     def test_prob_greedy_alg(self):
         self.assertEqual(self.gcn.greedyAlg(self.gcn.f), set(['a', 'c']))
         self.gcn.g.add_edges_from([('c', 1), ('c', 4)])
         self.assertEqual(self.gcn.greedyAlg(self.gcn.f), set(['c']))
-        pass
 
     def choices(population, weights):
         if "a" in population:
@@ -47,4 +46,36 @@ class TestGraphConvNet(TestCase):
         self.gcn.g.add_edges_from([('a', 0), ('a', 3)])
         self.assertEqual(self.gcn.probalisticGreedyAlg(self.gcn.f)[0], set(['a']))
         self.assertEqual(self.gcn.probalisticGreedyAlg(self.gcn.f)[1]["a"], 5)
-    pass
+
+
+    def test_feature_degree(self):
+        self.assertEqual(self.gcn.feature_degree()['b'], 1/5)
+        self.assertEqual(self.gcn.feature_degree()['a'], 3/5)
+
+    def test_feature_mean_uni_degree(self):
+        self.assertEqual(self.gcn.feature_mean_uni_degrees()['b'], 3/3)
+        self.assertAlmostEqual(self.gcn.feature_mean_uni_degrees()['a'], ((5/3)/3))
+
+    def test_feature_min_uni_degree(self):
+        self.assertEqual(self.gcn.feature_min_uni_degrees()['b'], 3/3)
+        self.assertEqual(self.gcn.feature_min_uni_degrees()['a'], 1/3)
+
+    def test_features_to_tensors(self):
+        data, subset_to_row = self.gcn.features_to_tensors()
+        self.assertEqual(data[subset_to_row['a'], 0], 3/5)
+        self.assertAlmostEqual(data[subset_to_row['a'], 1], (5/3)/3)
+        self.assertEqual(data[subset_to_row['a'], 2], 1/3)
+
+    def mock_compute_marginal_gain(self, i):
+        return {'a': 1, 'b': 2, 'c': 3}
+
+    def test_labels_to_tensors(self):
+        data, subset_to_row = self.gcn.features_to_tensors()
+        self.gcn.compute_marginal_gain = self.mock_compute_marginal_gain
+        labels = self.gcn.compute_marginal_gain(1)
+        tensor_labels = self.gcn.labels_to_tensors(labels, subset_to_row)
+        self.assertEqual(tensor_labels[subset_to_row['a']], 1)
+        self.assertEqual(tensor_labels[subset_to_row['b']], 2)
+        self.assertEqual(tensor_labels[subset_to_row['c']], 3)
+
+
